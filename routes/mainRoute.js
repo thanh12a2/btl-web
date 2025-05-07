fca07da892f57551ff7496eb2efa87e97f255c1a
nam <29ngtrung@gmail.com>
2025-05-08 03:56:44 +0700

    Fix

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -100,6 +100,7 @@ router.get("/api/weather", async (req, res) => {
 });
 
 router.get("/backdetails", authController.authenticateToken, async (req, res) => {
+  // console.log(res.locals);
   const role = res.locals.role;
   if (role == "Admin") {
     const query = `SELECT * FROM [dbo].[User] WHERE email = @email`
